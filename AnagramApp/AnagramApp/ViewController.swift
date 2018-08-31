cb7c520ca1d59959338da23a2bcfd09c3e7323aa
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-08-31 11:06:15 -0400

    layout added

--- AnagramApp/AnagramApp/ViewController.swift
+++ AnagramApp/AnagramApp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordToBeDisplayedLabel: UILabel!
+    
+    var words = ["angel", "skate", "robes", "heroes", "mate"]
+    
+    wordToBeDisplayedLabel.text = words.random()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
